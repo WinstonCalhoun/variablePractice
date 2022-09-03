34b3ece5aa705df25baa3958af777a4f85da79e5
Winston Calhoun <superdevant@gmail.com>
2022-09-02 19:09:22 -0500

    add console.log

--- variables.js
+++ variables.js
@@ -1,8 +1,16 @@
 var email = "superdevant@gmail.com"
+console.log(email)
 var startingAge = 32
+console.log(startingAge)
 var depositPaid = false
+console.log(depositPaid)
 var name = "winston"
-var endAge = 65
+console.log(name)
+var retireAge = 65
+console.log(retireAge)
 var alive = true
-var alive = false
-var sentance = `${name} is my name`
+console.log(alive)
+var eSentence = "${email} users email";
+console.log(eSentence)
+var nSentence = "${name} first name of user";
+console.log(nSentence)
